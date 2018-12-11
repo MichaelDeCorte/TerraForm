7d4eb3e7b5113efed2dc8f474e64a0f90f6f1bac
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2018-12-11 12:47:10 -0500

    support for bastion servers and more robust chef script

--- instance/chef/instance.tf
+++ instance/chef/instance.tf
@@ -35,6 +35,11 @@ variable "run_list" {
     default = []
 }
 
+variable "security_groups" {
+    type = "list"
+    default = []
+}
+
 variable "depends_id" {
     type = "map"
     default = { }
@@ -53,29 +58,32 @@ variable "iam_instance_profile" {
     type = "string"
 }
 
-variable "associate_public_ip_address" {
-    default	= false
+
+############################################################
+# return variables
+output "private_dns" {
+    value = "${aws_instance.instance.private_dns}"
 }
 
+output "public_dns" {
+    value = "${aws_instance.instance.public_dns}"
+}
+
+output "instanceid" {
+    value = "${aws_instance.instance.id}"
+}
 
 ############################################################
 # module
 resource "aws_instance" "instance" {
-
-    lifecycle {
-        ignore_changes = [
-            "ami"
-        ]
-    }
-    
-    ami	   						= "${data.aws_ami.linux_ami.image_id}"
+    ami	   					= "${data.aws_ami.linux_ami.image_id}"
     instance_type 				= "${var.instance_type}"
     key_name 					= "${local.ssh_key["key_name"]}"
 
     tags 						= "${merge(var.depends_id,var.tags, 
 										var.globals["tags"],
 										map("Name", "${var.name}"))}"
-    associate_public_ip_address	= "${var.associate_public_ip_address}"
+    associate_public_ip_address	= true
 
     subnet_id					= "${var.subnet_id}"
     vpc_security_group_ids		= [ "${var.vpc_security_group_ids}" ]
@@ -101,37 +109,4 @@ resource "aws_instance" "instance" {
     # }
 
 }	 
-############################################################
-# return variables
 
-output "id" {
-    value = "${aws_instance.instance.id}"
-}
-
-output "arn" {
-    value = "${aws_instance.instance.arn}"
-}
-
-output "public_dns" {
-    value = "${aws_instance.instance.public_dns}"
-}
-
-output "public_ip" {
-    value = "${aws_instance.instance.public_ip}"
-}
-
-output "private_dns" {
-    value = "${aws_instance.instance.private_dns}"
-}
-
-output "private_ip" {
-    value = "${aws_instance.instance.private_ip}"
-}
-
-output "availability_zone" {
-    value = "${aws_instance.instance.availability_zone}"
-}
-
-output "subnet_id" {
-    value = "${aws_instance.instance.subnet_id}"
-}
