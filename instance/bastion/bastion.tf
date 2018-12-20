7cdfaba6920994f570c324e110bd1a5f99177726
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2018-12-20 10:57:05 -0500

    fixed bastion dns

--- instance/bastion/bastion.tf
+++ instance/bastion/bastion.tf
@@ -173,7 +173,7 @@ resource "aws_route53_record" "bastion_dns" {
 
     zone_id = "${var.zone_id}"
     name    = "${var.name}"
-    type    = "CNAME"
+    type    = "A"
     ttl     = "300"
     records = [ "${module.bastion_host.public_ip}" ]
 }
