97e9bef944011b1f6b18200dc419407adff39749
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2019-01-21 09:30:57 -0500

    fixed bug in snapshots

--- instance/snapshots/snapshots.tf
+++ instance/snapshots/snapshots.tf
@@ -197,7 +197,7 @@ resource "aws_dlm_lifecycle_policy" "dlm_policy" {
         }
 
         target_tags {
-            Snapshot = true
+            Snapshot = "${local.region["env"]}
         }
     }
 }
