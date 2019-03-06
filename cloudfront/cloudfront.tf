aa0178ff284394e645edd004ff4fefe741b2bb84
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2019-03-06 11:16:51 -0500

    cloudfront support for max_ttl

--- cloudfront/cloudfront.tf
+++ cloudfront/cloudfront.tf
@@ -89,7 +89,7 @@ resource "aws_cloudfront_distribution" "cloudfront" {
         viewer_protocol_policy = "redirect-to-https"
         min_ttl                = 0
         default_ttl            = "${var.default_ttl}"
-        max_ttl                = 360 # "${var.default_ttl}"
+        max_ttl                = "${var.default_ttl}"
         compress               = false
     }
 
