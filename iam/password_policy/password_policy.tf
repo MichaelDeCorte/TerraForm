bd61cdc6746ee93c4deb287f6d3cdf53d253f8b5
Michael DeCorte <mdecorte.decorte+github@gmail.com>
2019-05-01 10:36:55 -0400

    iam-password-policy

---
+++ iam/password_policy/password_policy.tf
@@ -0,0 +1,78 @@
+# include "global" variables
+variable "globals" {
+    type = "map"
+}
+
+variable "tags" {
+	 type = "map"
+	 default = { }
+}
+
+variable "allow_users_to_change_password" {
+    default = "true"
+}
+
+variable "hard_expiry" {
+    default = "false"
+}
+
+variable "max_password_age" {
+    default = "0"
+}
+
+variable "minimum_password_length" {
+    default = "8"
+}
+
+variable "password_reuse_prevention" {
+    default = "10"
+}
+
+variable "require_lowercase_characters" {
+    default = "true"
+}
+
+variable "require_numbers" {
+    default = "true"
+}
+
+variable "require_symbols" {
+    default = "true"
+}
+
+variable "require_uppercase_characters" {
+    default = "true"
+}
+
+
+############################################################
+
+resource "aws_iam_account_password_policy" "policy" {
+    allow_users_to_change_password = "${var.allow_users_to_change_password}"
+    hard_expiry = "${var.hard_expiry}"
+    max_password_age = "${var.max_password_age}"
+    minimum_password_length = "${var.minimum_password_length}"
+    password_reuse_prevention = "${var.password_reuse_prevention}"
+    require_lowercase_characters = "${var.require_lowercase_characters}"
+    require_numbers = "${var.require_numbers}"
+    require_symbols = "${var.require_symbols}"
+    require_uppercase_characters = "${var.require_uppercase_characters}"
+}
+
+############################################################
+output "expire_passwords" {
+    value = "${aws_iam_account_password_policy.policy.expire_passwords}"
+}
+
+############################################################
+# hack for lack of depends_on
+
+variable "depends" {
+    default = ""
+}
+
+output "depends" {
+    value   = "${var.depends}:iam/password_policy:${aws_iam_account_password_policy.policy.expire_passwords}"
+}
+
+
